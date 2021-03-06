ef5423ac1d0531028faf4495484e5197be5536e9
Marsallis <MarsallisF@gmail.com>
2021-03-06 13:12:41 -0500

    working player code

--- players.rb
+++ players.rb
@@ -2,15 +2,10 @@ class Player
     attr_accessor :name, :bankroll, :total
     attr_reader :cards
 
-    def start_game
-        @cards = []
-        @total = 0
-        @bankroll = 100
+    def initialize name
         @name = name
     end
-    def get_cards(card)
-        @cards << card
-        @total += card.value
-      end
-    
 end
+
+human = Player.new "Marsallis"
+p human
