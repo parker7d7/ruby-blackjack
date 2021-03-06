cdcb4b2b494b9062ed22d26a1c8aa454a64bdf87
Ryan Reiterman <ryreiterman@gmail.com>
2021-03-06 13:08:18 -0500

    Player class works

--- players.rb
+++ players.rb
@@ -1,16 +1,22 @@
 class Player
-    attr_accessor :name, :bankroll, :total
+    attr_accessor :name, :bankroll, :hand_total
     attr_reader :cards
 
-    def start_game
+    def initialize name, bankroll
         @cards = []
-        @total = 0
-        @bankroll = 100
+        @hand_total = 0
         @name = name
+        @bankroll = bankroll
+        
     end
-    def get_cards(card)
-        @cards << card
-        @total += card.value
-      end
+    # def get_cards(card)
+    #     @cards << card
+    #     @hand_total += card.value
+    #   end
     
 end
+
+human = Player.new "Ryan", 100
+dealer = Player.new "Dealer", 10000
+p human
+p dealer
