88b265e0577523a93ab8f516db7683dbf2d8744b
Allegra✨ <amhilburger@gmail.com>
2021-03-09 15:53:53 -0700

    Create a case to compare card totals and choose winner

--- blackjack_notes.rb
+++ blackjack_notes.rb
@@ -137,7 +137,47 @@
         
         ##consider writing a switch case statement to cover all situations
 
-#joe commit test goat
-#goat squad
 
-#andrea was here!
+
+#Parkers total code:
+humanTotal = human.cards.reduce(0){|sum, card| sum + card.value} 
+computerTotal = computer.cards.reduce(0){|sum, card| sum + card.value}
+
+#Allegra's switch case:
+# we need a tie situation, a bust situation (over 21) for both players, and a winning situtation for both players, under 21 and a loosing situation with both ability to replay and inability to replay.
+# It could be neat to have the 'pot' where the 100 from each player goes into an array so if there is a tie, the next round increments by another 100.
+# Should we break this switch case up into multiple cases? 1. for winning/losing, 2. tie, 3. bust?
+# Is the syntax right here?
+
+case game_Outcome
+when humanTotal > computerTotal && humanTotal <= 21
+    Player.bankroll += 100
+    Computer.bankroll -= 100
+    p "You win! Don't stop now, play again?"
+    new_deck.shuffle
+when computerTotal > humanTotal && computerTotal <= 21 && Player.bankroll >= 100
+    Computer.bankroll += 100
+    Player.bankroll -= 100
+    p "No luck this time. But it's worth another try!"
+    new_deck.shuffle
+when computerTotal > humanTotal && computerTotal <= 21 && Player.bankroll <= 100
+    Computer.bankroll += 100
+    Player.bankroll -= 100
+    p "Time to cool it, your out of money. Better luck next time!"
+    #restart
+when computerTotal == humanTotal && computerTotal <=21
+    p "Close but a tie. Let's draw a new hand."
+    new_deck.shuffle
+when humanTotal > 21 && Player.bankroll >= 100
+    Computer.bankroll += 100
+    Player.bankroll -= 100
+    p 'Your total is #{humanTotal}. It is a bust. Try again!'
+when computerTotal > 21
+    Player.bankroll += 100
+    Computer.bankroll -= 100
+    p 'The dealer went bust. Money is all yours!'
+else humanTotal > 21 && Player.bankroll < 100
+    Computer.bankroll += 100
+    Player.bankroll -= 100
+    p 'Your out of money. Take a trip to the bank or maybe sleep this one off.'
+
